65c90c983f5c3186310b46ee950ab8ae3d776e6b
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-11-06 13:00:17 +0100

    Add GridDataWithPercentage

---
+++ INA_lab3/GridDataWithPercentage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INA_lab3
+{
+    internal class GridDataWithPercentage
+    {
+        public int N { get; set; }
+        public double XReal { get; set; }
+        public string XBin { get; set; }
+        public double GxReal { get; set; }
+        public double Percentage { get; set; }
+    }
+}
