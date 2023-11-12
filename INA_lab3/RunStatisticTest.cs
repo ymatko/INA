3510b8302106312568fec319bf2dff496b8d9121
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-11-12 17:13:39 +0100

    Add RunStatisticTest class

---
+++ INA_lab3/RunStatisticTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INA_lab3
+{
+    internal class RunStatisticTest
+    {
+        public int N { get; set; }
+        public int T { get; set; }
+        public double Pk { get; set; }
+        public double Pm { get; set; }
+        public double Fx { get; set; }
+
+        internal void ViewData(DataGridView dataGridView)
+        {
+            dataGridView.Rows.Add(N, Pk, Pm, T, Fx) ;
+        }
+    }
+}
