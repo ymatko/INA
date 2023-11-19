58cfc1dd51e460cbe4fe08a69aa1a851b0241ace
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-11-19 21:51:06 +0100

    Add RandBin

---
+++ INA_lab4/RandBin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INA_lab4
+{
+    internal static class RandBin
+    {
+        internal static string GetValue(double a, double b, double d)
+        {
+            int l = (int)Math.Floor(Math.Log((b - a) / d, 2) + 1.0);
+            var result = new StringBuilder();
+            var rand = new Random();
+            for(int i = 1; i < l; i++)
+            {
+                result.Append(rand.Next(2));
+            }
+            return result.ToString();
+        }
+    }
+}
