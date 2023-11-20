a2b1ad47a830ae37e1e75a81876cc04e1be1b730
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-11-20 22:06:10 +0100

    Add Calculate

--- INA_lab4/MainForm.cs
+++ INA_lab4/MainForm.cs
@@ -16,14 +16,25 @@ namespace INA_lab4
 
         private void btnCalc_Click(object sender, EventArgs e)
         {
-            //int t = Convert.ToInt32(tbT.Text);
-            //double a = Convert.ToDouble(tbA.Text);
-            //double b = Convert.ToDouble(tbB.Text);
-            //double d = Convert.ToDouble(cbD.Text);
+            int t = Convert.ToInt32(tbT.Text);
+            double a = Convert.ToDouble(tbA.Text);
+            double b = Convert.ToDouble(tbB.Text);
+            double d = Convert.ToDouble(cbD.Text);
             //for (int i = 0; i < t; i++)
             //{
             //    dataGridView1.Rows.Add(RandBin.GetValue(a, b, d));
             //}
+            Calculate(t, a, b, d);
+        }
+
+        private void Calculate(int t, double a, double b, double d)
+        {
+            for (int i = 0; i < t; i++)
+            {
+                MainObject obj = new MainObject(a, b, d);
+                obj.xBin = RandBin.GetValue(a, b, d);
+                var listOfobj = obj.GetDescendants();
+            }
         }
     }
 }
