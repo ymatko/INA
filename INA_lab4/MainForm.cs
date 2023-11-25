452f132906fb276f4a6fb846305c788c9c160e4b
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-11-25 21:27:30 +0100

    Add main line

--- INA_lab4/MainForm.cs
+++ INA_lab4/MainForm.cs
@@ -36,32 +36,47 @@ namespace INA_lab4
             tbBestXbin.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
             tbBestFxReal.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
         }
-
+        
         private List<string> Calculate()
         {
             List<string> newObj = new List<string>();
             formsPlot1.Plot.Clear();
+            double[] mainLine = new double[_t];
+            double[] index = new double[_t];
+            for (int k = 0; k < index.Length; k++)
+            {
+                index[k] += k;
+            }
             for (int i = 0; i < _t; i++)
             {
                 MainObject obj = new MainObject(_a, _b, _d);
                 obj._xBin = RandBin.GetValue(_a, _b, _d);
                 var listOfobj = obj.GetDescendants();
                 double[] allXbin = new double[listOfobj.Count];
+                
                 for (int j = 0; j < listOfobj.Count; j++)
                 {
                     string str = listOfobj[j];
+                    
                     if (GetFx(GetxReal(obj._xBin)) < GetFx(GetxReal(str)))
                     {
                         obj._xBin = str;
                     }
-                    //formsPlot1.Plot.AddPoint(i, GetFx(GetxReal(obj._xBin)));
                     allXbin[j] = GetFx(GetxReal(obj._xBin));
                 }
-                formsPlot1.Plot.AddLine(i, allXbin.Min(), i, allXbin.Max());
+
+                mainLine[i] = GetFx(GetxReal(obj._xBin));
+                if (i > 0 && mainLine[i] < mainLine[i - 1])
+                {
+                    mainLine[i] = mainLine[i - 1];
+                }
+
+                formsPlot1.Plot.AddLine(i, allXbin.Min(), i, allXbin.Max(), color: Color.Blue);
                 newObj.Add(obj._xBin);
-                //formsPlot1.Plot.AddPoint(i, GetFx(GetxReal(obj._xBin)));
 
             }
+            formsPlot1.Plot.AddScatter(index, mainLine, color: Color.Red, lineWidth: 3);
+            formsPlot1.Plot.AxisAuto();
             formsPlot1.Render();
             return newObj;
         }
