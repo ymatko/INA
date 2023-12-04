5c50691e54704e99a0d2748bad6d57eb0710ed8a
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-04 12:00:25 +0100

    Add Particle

---
+++ INA_lab5/Particle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INA_lab5
+{
+    internal class Particle
+    {
+        internal double Position { get; set; }
+        internal double Speed { get; set; }
+        internal double BestOfParticle { get; set; }
+        internal double BestOfNeighbors { get; set; }
+    }
+}
