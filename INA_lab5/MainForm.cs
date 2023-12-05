b8c73ae23026369f7108dd19ced9717d8d541fd5
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-12-05 16:39:57 +0100

    Add all logic

--- INA_lab5/MainForm.cs
+++ INA_lab5/MainForm.cs
@@ -28,7 +28,28 @@ namespace INA_lab5
             _n = Convert.ToInt32(textBox_N.Text);
             _t = Convert.ToInt32(textBox_T.Text);
             CreateParticles();
+            Calculate();
         }
+
+        private void Calculate()
+        {
+            for(int i = 0; i < _t; i++)
+            {
+                for(int j = 0; j < particles.Count; j++)
+                {
+                    if (Fx(particles[j].Position) > Fx(particles[j].BestOfParticle))
+                        particles[j].BestOfParticle = particles[j].Position;
+                    if (Fx(particles[j].Position) > Fx(particles[j].BestOfNeighbors))
+                        particles[j].BestOfNeighbors = particles[j].Position;
+                }
+                foreach(Particle obj in particles)
+                {
+                    obj.Speed = CalculateSpeed(obj);
+                    obj.Position += obj.Speed;
+                }
+            }
+        }
+
         private void CreateParticles()
         {
             particles.Clear();
@@ -38,6 +59,18 @@ namespace INA_lab5
                 obj.Position = rand.NextDouble() * (_b - _a) + _a;
                 particles.Add(obj);
             }
+        }
+
+        private double Fx(double value)
+        {
+            return (value % 1.0) * (Math.Cos(20.0 + Math.PI + value)) - Math.Sin(value);
+        }
+
+        private double CalculateSpeed(Particle particle)
+        {
+            return _c1 * rand.NextDouble() * particle.Speed + 
+                _c2 * rand.NextDouble() * (particle.BestOfParticle - particle.Position) + 
+                _c3 * rand.NextDouble() * (particle.BestOfNeighbors - particle.Position);
         }
     }
 }
